8d0c0841615c623aeb88341c21f4681b6f83733f
oucyz <dks.9886.po@gmail.com>
2025-08-02 23:46:32 +0900

    refac: use linear

--- src/models/self_attention.py
+++ src/models/self_attention.py
@@ -2,22 +2,22 @@ import torch
 import torch.nn as nn
 
 
-class SelfAttentionV1(nn.Module):
-    def __init__(self, d_in: int, d_out: int):
+class SelfAttentionV2(nn.Module):
+    def __init__(self, d_in: int, d_out: int, bias: bool = False):
         super().__init__()
-        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
-        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
-        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_query = nn.Linear(d_in, d_out, bias=bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        query = x @ self.W_query  # 系列長 x d_out
-        key = x @ self.W_key  # 系列長 x d_out
+        query = self.W_query(x)  # 系列長 x d_out
+        key = self.W_key(x)  # 系列長 x d_out
         attention_weights = query @ key.T  # 系列長 x 系列長
         normalized_attention_weights = torch.softmax(
             attention_weights / key.shape[-1] ** 0.5, dim=-1
         )
 
-        value = x @ self.W_value  # 系列長 x d_out
+        value = self.W_value(x)  # 系列長 x d_out
         context_vec = normalized_attention_weights @ value  # 系列長 x d_out
         return context_vec
 
@@ -29,6 +29,6 @@ if __name__ == "__main__":
     embedding = torch.tensor(torch.rand(n, d_in), dtype=torch.float32)
     print(embedding.shape)  # torch.Size([6, 8])
 
-    self_attention = SelfAttentionV1(d_in=d_in, d_out=d_out)
+    self_attention = SelfAttentionV2(d_in=d_in, d_out=d_out, bias=False)
     output = self_attention(embedding)
     print(output)
