d29135a7fef2f41d34bc0f4bd9ba53f32565f1a9
oucyz <dks.9886.po@gmail.com>
2025-08-02 23:38:04 +0900

    feat: self attention

---
+++ src/models/self_attention.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+
+class SelfAttentionV1(nn.Module):
+    def __init__(self, d_in: int, d_out: int):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        query = x @ self.W_query  # 系列長 x d_out
+        key = x @ self.W_key  # 系列長 x d_out
+        attention_weights = query @ key.T  # 系列長 x 系列長
+        normalized_attention_weights = torch.softmax(
+            attention_weights / key.shape[-1] ** 0.5, dim=-1
+        )
+
+        value = x @ self.W_value  # 系列長 x d_out
+        context_vec = normalized_attention_weights @ value  # 系列長 x d_out
+        return context_vec
+
+
+if __name__ == "__main__":
+    n = 6
+    d_in = 8
+    d_out = 8
+    embedding = torch.tensor(torch.rand(n, d_in), dtype=torch.float32)
+    print(embedding.shape)  # torch.Size([6, 8])
+
+    self_attention = SelfAttentionV1(d_in=d_in, d_out=d_out)
+    output = self_attention(embedding)
+    print(output)
